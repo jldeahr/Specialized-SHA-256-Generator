ee8d6cfdfeea6a5ba91102ed7eb2590e65d91814
jldeahr <jldeahr@NN123-HA-12.HS.sd203.org>
2015-04-08 12:21:06 -0500

    dafsd

--- Random4.java
+++ Random4.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.ArrayList;
 
-public class Random16
+public class Random4
 {
     // initialize array a with all possible values
     static String[] a = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
@@ -13,7 +13,7 @@ public class Random16
         ArrayList<String> f = new ArrayList<String>();
 
         int h = 0;
-        while (h < 9999999)
+        while (h < 14776336)
         {
             for (int i = 0; i < a.length; i++)
             {
@@ -23,56 +23,20 @@ public class Random16
                     {
                         for (int l = 0; l < a.length; l++)
                         {
-                            for (int m = 0; m < a.length; m++)
-                            {
-                                for (int n = 0; n < a.length; n++)
-                                {
-                                    for (int o = 0; o < a.length; o++)
-                                    {
-                                        for (int p = 0; p < a.length; p++)
-                                        {
-                                            for (int q = 0; q < a.length; q++)
-                                            {
-                                                for (int r = 0; r < a.length; r++)
-                                                {
-                                                    for (int s = 0; s < a.length; s++)
-                                                    {
-                                                        for (int t = 0; t < a.length; t++)
-                                                        {
-                                                            for (int u = 0; u < a.length; u++)
-                                                            {
-                                                                for (int v = 0; v < a.length; v++)
-                                                                {
-                                                                    for (int w = 0; w < a.length; w++)
-                                                                    {
-                                                                        for (int x = 0; x < a.length; x++)
-                                                                        {
-                                                                            f.add(h,a[i] + a[j] + a[k] + a[l] + a[m] + a[n] + a[o] + a[p] + a[q] + a[r] + a[s] + a[t] + a[u] + a[v] + a[w] + a[x]);
-                                                                            //System.out.println(a[i] + a[j] + a[k] + a[l] + a[m] + a[n] + a[o] + a[p] + a[q] + a[r] + a[s] + a[t] + a[u] + a[v] + a[w] + a[x]);
-                                                                            System.out.println(f.get(h));
-                                                                            h++;
-                                                                            System.out.println(h);
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            f.add(h,a[i] + a[j] + a[k] + a[l]);
+                            //System.out.println(a[i] + a[j] + a[k] + a[l] + a[m] + a[n] + a[o] + a[p] + a[q] + a[r] + a[s] + a[t] + a[u] + a[v] + a[w] + a[x]);
+                            System.out.println(f.get(h));
+                            h++;
+                            System.out.println(h);
                         }
                     }
                 }
             }
         }
-        
+
         return f;
     }
-    
+
     public static String[] combos2()
     {
         String[] f = new String[999999];
@@ -133,10 +97,10 @@ public class Random16
                 }
             }
         }
-        
+
         return f;
     }
-    
+
     public static void main(String[] args)
     {
         combos1();
